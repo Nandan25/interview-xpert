30372d1655a12e9dfda345512b1057f018fea29f
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-07-08 13:28:59 +0530

    removed timeout for axios

--- client/interview-xpert/src/utils/axiosInstance.js
+++ client/interview-xpert/src/utils/axiosInstance.js
@@ -4,7 +4,6 @@ import { API_BASE_URL } from './apiPaths';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
-    timeout: 120000,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
