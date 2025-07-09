3aa581a5a392a8d1316b3da5ba8086b761a3e465
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-07-09 11:28:43 +0530

    removed unused imports

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/User";
-import { error } from "console";
 import { AppLogger } from "../utils/logger";
 
 // Middleware to protect routes
